f6a61db116772f410815d4c220b9504b79877d46
HollowReaper77 <dtomibite@gmail.com>
2025-11-28 22:29:59 +0100

    film létrehozás implementálva

---
+++ api/create.php
@@ -0,0 +1,36 @@
+<?php
+    //headers
+
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    header('Acces-Control-Allow-Methods: POST');
+    header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization,X-Requested-With');
+
+    // előkészíteni az api-t
+    include_once('../core/initialize.php');
+
+
+    // a film előkészítése
+    $film = new Film($dbConn);
+
+
+    $data = json_decode(file_get_contents("php://input"));
+
+    $film->film_id = $data->film_id;
+    $film->cim = $data->cim;
+    $film->idotartam = $data->idotartam;
+    $film->poszter_url = $data->poszter_url;
+    $film->leiras = $data->leiras;
+    $film->kiadas_ev = $data->kiadas_ev;
+
+
+    if($film->create()){
+        echo json_encode(
+            array('message' => 'Film létrehozva.')
+        );
+    }else{
+        echo json_encode(
+            array('message' => 'A film nem lett létrehozva.')
+        );
+    }
+?>
--- core/film.php
+++ core/film.php
@@ -63,13 +63,38 @@ class Film{
         $this->kiadas_ev =$row['kiadasev'];
 
         return $stmt;
+    }
+
+    public function create(){
+        $query = 'INSERT INTO ' .$this->table. 'SET film_id = : film_id, cim = : cim, idotartam = : idotartam, poszt_url = : poszt_url, leiras = : leiras, kiadas_ev = : kiadas_ev';
+        $stmt = $this->conn->prepare($query);
+
+        $this->film_id =        htmlspecialchars(strip_tags($this->film_id));
+        $this->cim =            htmlspecialchars(strip_tags($this->cim));
+        $this->idotartam =      htmlspecialchars(strip_tags($this->idotartam));
+        $this->poszter_url =    htmlspecialchars(strip_tags($this->poszter_url));
+        $this->leiras =         htmlspecialchars(strip_tags($this->leiras));
+        $this->kiadas_ev =      htmlspecialchars(strip_tags($this->kiadas_ev));
 
 
+        $stmt->bindParam(':film_id', $this->film_id);
+        $stmt->bindParam(':cim', $this->cim);
+        $stmt->bindParam(':idotartam', $this->idotartam);
+        $stmt->bindParam(':poszter_url', $this->poszter_url);
+        $stmt->bindParam(':leiras', $this->leiras);
+        $stmt->bindParam(':kiadas_ev', $this->kiadas_ev);
 
+        if($stmt->execute()){
+            return true;
+        }
 
-
+        printf("Error %s \n", $stmt->error);
+        return false;
 
     }
+
+
+
 }
 
 
