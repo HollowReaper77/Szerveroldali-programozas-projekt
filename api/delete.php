7bd6537948e69ed55037e15e9306de0707402dea
HollowReaper77 <dtomibite@gmail.com>
2025-11-29 21:14:27 +0100

    delete fájlban a header elírása javítva

--- api/delete.php
+++ api/delete.php
@@ -3,7 +3,7 @@
 
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
-    header('Acces-Control-Allow-Methods: DELETE');
+    header('Access-Control-Allow-Methods: DELETE');
     header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization,X-Requested-With');
 
     // előkészíteni az api-t
