c105722f15bf4ab133a3d48f8a544ca0745fae00
HollowReaper77 <dtomibite@gmail.com>
2025-11-29 21:36:41 +0100

    útvonal javítások az initialize-ban, az include mappának a nevét kiegészítettem egy 's'-el

--- core/initialize.php
+++ core/initialize.php
@@ -4,7 +4,7 @@
     defined('SITE_ROOT') ? null: define('SITE_ROOT', DS. 'wamp64'.DS.'www'.DS.'film');
     //wamp64/www/phprest/includes
     defined('INC_PATH') ? null : define('INC_PATH', SITE_ROOT.DS.'includes');
-    defined('CORE_PATH') ? null : define('CORE_PATH', SITE_ROOT.DS.'includes');
+    defined('CORE_PATH') ? null : define('CORE_PATH', SITE_ROOT.DS.'includes'.DS.'core');
 
     // betölti a configurációs fájtl elsőnek
     require_once(INC_PATH.DS."config.php");
